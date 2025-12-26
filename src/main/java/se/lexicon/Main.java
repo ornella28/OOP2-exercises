b36a56f6e77d383575238197e5cd4a16042b43c0
ornella28 <angelnibaruta88@gmail.com>
2025-12-26 01:11:17 +0100

    checking the print out

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,6 +1,22 @@
 package se.lexicon;
+
+
 public class Main {
-    static void main() {
+
+
+     void main() {
+
+         Service summerService = new SummerTireChange(1, "Summer Tire Change", 500);
+         Service winterService = new WinterTireChange(2, "Winter Tire Change", 600);
+
+            System.out.println(summerService);
+            System.out.println("Duration: " + summerService.getDurationInMinutes() + " minutes");
+            System.out.println("Price: $" + summerService.calculatePrice());
+            System.out.println();
+            System.out.println(winterService);
+            System.out.println("Duration: " + winterService.getDurationInMinutes() + " minutes");
+            System.out.println("Price: $" + winterService.calculatePrice());
+
 
 
     }
